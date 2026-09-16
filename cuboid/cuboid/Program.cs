fa3d7bd1c04eca499ba93586478cdc4d341acbae
Shahfarhan26 <2468shahfarhan@gmail.com>
2026-09-16 21:08:03 +0530

    star o=pattern problems

---
+++ cuboid/cuboid/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //cuboid staar pattern.
+        for (int i = 0 ; i < 5; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                Console.Write("*\t");
+            }
+            Console.WriteLine("*\t");
+        }
+        //right angled triangle star pattern.
+        Console.Write("\n");
+        Console.Write("\n");
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < i + 1; j++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine("");
+        }
+        //triangle star pattern.
+        Console.Write("\n");
+        Console.Write("\n");
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10 - i - 1; j++)
+            {
+                Console.Write(" ");
+            }
+            for (int k = 0; k < i * 2 + 1; k++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
