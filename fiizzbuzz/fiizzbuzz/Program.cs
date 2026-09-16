2d9cf31285e005c44662b4bd3cba07edad4b44fd
Shahfarhan26 <2468shahfarhan@gmail.com>
2026-09-16 21:20:44 +0530

    FizzBuzz problem

---
+++ fiizzbuzz/fiizzbuzz/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i < 101; i++)
+        {
+            if ((i % 3) == 0 && (i % 5 ) == 0)
+            {
+                Console.Write("FizzBuzz\t");
+                continue;
+            }else
+            {
+                if ((i % 3) == 0)
+                {
+                    Console.Write("Fizz\t");
+                    continue;
+                }
+                if ((i % 5) == 0)
+                {
+                    Console.Write("Buzz\t");
+                    continue;
+                }
+            }
+            
+
+            Console.Write(i + "\t");
+        }
+    }
+}
