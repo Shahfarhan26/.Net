54cca07668f5cdeeafb81bd6a088539694faad53
Shahfarhan26 <2468shahfarhan@gmail.com>
2026-09-17 22:08:36 +0530

    Number Guessing Game

---
+++ NumberGuessingGame/NumberGuessingGame/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //number guessing game.
+        Console.WriteLine("Welcome TO The Number Guessing Game.\nHope You have Fun.");
+        Random random = new Random();
+        
+        bool Playgame = true;
+        while (Playgame)
+        {
+            int r = random.Next(1, 101);
+            Console.WriteLine("Choose A number Between 1 and 100");
+            int n = Convert.ToInt32(Console.ReadLine());
+            while (n != r) {
+                if (n > r)
+                {
+                    Console.WriteLine("You have guessed a high number .Try again");
+                }
+                else if (n < r)
+                {
+                    Console.WriteLine("You have guessed a low number .Try again");
+                }
+                n = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("You have won. it was indeed " + r);
+            Console.WriteLine("Do You Want To Play Again(Yes/No)");
+            string res = Console.ReadLine().ToLower();
+            if (res == "yes")
+            {
+                Playgame = true;
+            }
+            else
+            {
+                Playgame = false;
+            }
+        }
+        Console.WriteLine("Thanks for playing!");
+    }
+}
