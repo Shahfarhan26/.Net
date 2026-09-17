fd7bf54217d0141270c3191785337c4a2937f8e6
Shahfarhan26 <2468shahfarhan@gmail.com>
2026-09-17 21:03:48 +0530

    Square array

--- arrayOfArrays/arrayOfArrays/Program.cs
+++ arrayOfArrays/arrayOfArrays/Program.cs
@@ -7,10 +7,10 @@ class Program
     {
         //HOW TO WRITE jagged matrix arrays.
         int[][] arr1 = new int[4][]; 
-        arr1[0] = new int[5];
+        arr1[0] = new int[3];
         arr1[1] = new int[2];
         arr1[2] = new int[3];
-        arr1[3] = new int[7];
+        arr1[3] = new int[1];
         Console.WriteLine("Enter Numbers: ");
         //take input from user.
         for (int i = 0; i < arr1.Length; i++)
@@ -29,5 +29,27 @@ class Program
             }
             Console.WriteLine();
         }
+
+        //square array.
+        Console.WriteLine("Enter Numbers: ");
+        int[,] Sarr = new int[3,3]; 
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Sarr[i,j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        Console.WriteLine("Numbers in The Matrix are: ");
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write(Sarr[i,j] + " ");
+            }
+            Console.WriteLine();
+        }
+
     }
 }
