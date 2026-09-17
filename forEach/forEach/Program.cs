a31621fe52601b1de5dbef8c35f333532b39d020
Shahfarhan26 <2468shahfarhan@gmail.com>
2026-09-17 21:22:11 +0530

    foreach

---
+++ forEach/forEach/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr1 = new[] { 4, 2, 3 ,1 };
+        int min = int.MaxValue;
+        int total = 0;
+        
+        foreach (int i in arr1) {
+            if (min > i)
+            {
+                min = i;
+            }
+            total += i;
+        }
+            
+        Console.WriteLine("\nmin: " + min);
+        Console.WriteLine("average: " + (float)total/arr1.Length);
+    }
+}
