fbd26dd146751154baab1d97330d82bfee0be970
Shahfarhan26 <2468shahfarhan@gmail.com>
2026-09-17 12:14:26 +0530

    printimg an array

---
+++ minarray/minarray/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = new [] {49,23,1,2,3,56,4,78,5,6,7,3};
+        Console.Write("the array befor sorting:  ");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+        
+        
+    }
+}
