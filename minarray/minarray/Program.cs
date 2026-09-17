f4e5794c49f1c3f83ec105e46dc9f4c02f8f6825
Shahfarhan26 <2468shahfarhan@gmail.com>
2026-09-17 16:30:24 +0530

    find average

--- minarray/minarray/Program.cs
+++ minarray/minarray/Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[] arr = new [] {49,-23,-1,2,3,156,-4,-78,5,6,7,-3};
+        int[] arr = new[] { 49, -23, -1, 2, 3, 156, -4, -78, 5, 6, 7, -3 };
         //how to print array.
         Console.Write("the array before sorting:  ");
         for (int i = 0; i < arr.Length; i++)
@@ -24,7 +24,7 @@ class Program
         }
         Console.WriteLine("\nmin value: " + minValue);
 
-        //how to find the smalest value.
+        //how to find the largest value.
         int maxValue = int.MinValue;
         for (int i = 0; i < arr.Length; i++)
         {
@@ -33,8 +33,17 @@ class Program
                 maxValue = arr[i];
             }
         }
-        Console.Write("max value: " + maxValue);
+        Console.WriteLine("max value: " + maxValue);
 
-        
+        // how to find the average of an array
+        int total = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            total += arr[i];
+        }
+        Console.Write("sum: " + total);
+        float average = (float)total / arr.Length;
+        Console.WriteLine("sum: " + total);
+        Console.Write("average: " + average);
     }
 }
