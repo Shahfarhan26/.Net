92fa3d253d5f7b68d27ecdfc5cebd9cb423e5e6c
Shahfarhan26 <2468shahfarhan@gmail.com>
2026-09-19 12:17:03 +0530

    methods overloading

--- method/method/Program.cs
+++ method/method/Program.cs
@@ -8,6 +8,9 @@ class Program
         Console.WriteLine("whose the birthday boy/girl?");
         string? name = Console.ReadLine();
         singHappyBirthday(name);
+
+        Console.WriteLine("sum of 1 + 2 is " + add(1, 2));
+        Console.WriteLine("sum of 1 + 2 + 3 is " + add(1, 2,3));
     }
     static void singHappyBirthday(string name)
     {
@@ -19,4 +22,10 @@ class Program
         Console.WriteLine("May GOD bless you");
         Console.WriteLine("Happy Birthday to you");
     }
+    static int add(int a, int b)
+    {
+        return a + b;
+    }
+    static int add(int a, int b, int c) {  return a + b + c; } 
+
 }
