46324ab3eec53b16e88584d7f4dbbd2088064730
Shahfarhan26 <2468shahfarhan@gmail.com>
2026-09-19 12:00:56 +0530

    methods and args

---
+++ method/method/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("whose the birthday boy/girl?");
+        string? name = Console.ReadLine();
+        singHappyBirthday(name);
+    }
+    static void singHappyBirthday(string name)
+    {
+        Console.WriteLine("Happy Birthday to you");
+        Console.WriteLine("Happy Birthday to you");
+        Console.WriteLine("Happy Birthday to  " + name);
+        Console.WriteLine("Happy Happy Birthday to you");
+        Console.WriteLine("May GOD bless you");
+        Console.WriteLine("May GOD bless you");
+        Console.WriteLine("Happy Birthday to you");
+    }
+}
