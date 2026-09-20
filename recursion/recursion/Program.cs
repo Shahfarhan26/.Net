66baab58f6bef35c13837de3fb10cfa209ad1c1e
Shahfarhan26 <2468shahfarhan@gmail.com>
2026-09-20 13:15:42 +0530

    fibonacci series

---
+++ recursion/recursion/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = 10;
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine(fibo(i));
+        }
+        
+    }
+    static ulong fibo(int n)
+    {
+        if (n == 1) { return 1; }
+        else if (n == 2) { return 1; }
+        else
+        {
+            return fibo(n - 1) + fibo(n - 2);
+        }
+    }
+}
