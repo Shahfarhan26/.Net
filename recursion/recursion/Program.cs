16171e3598b5eca939e111d4ae9cb3d8987c69a2
Shahfarhan26 <2468shahfarhan@gmail.com>
2026-09-20 13:27:42 +0530

    factorial

--- recursion/recursion/Program.cs
+++ recursion/recursion/Program.cs
@@ -6,11 +6,17 @@ class Program
     static void Main(string[] args)
     {
         int n = 10;
+        Console.WriteLine("fibonacci series: ");
         for (int i = 1; i <= n; i++)
         {
             Console.WriteLine(fibo(i));
         }
-        
+        Console.WriteLine("Factorial: ");
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine(factorial(i));
+        }
+
     }
     static ulong fibo(int n)
     {
@@ -20,5 +26,13 @@ class Program
         {
             return fibo(n - 1) + fibo(n - 2);
         }
+    }
+    static ulong factorial(int n)
+    {
+        if (n == 0 || n == 1)
+        {
+            return 1;
+        }
+        return ((ulong)n * factorial(n - 1));
     }
 }
