6fb637979e28357f1227e86b03252d3304294d64
Shahfarhan26 <2468shahfarhan@gmail.com>
2026-09-21 22:10:20 +0530

    color class

---
+++ classes/classes/Colors.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public class Colors
+{
+    private int red, green, blue, alpha;
+
+    public Colors(int r, int g, int b, int a)
+    {
+        this.red = r;
+        this.green = g;
+        this.blue = b;
+        this.alpha = a;
+    }
+    public Colors(int r, int g, int b)
+    {
+        this.red = r;
+        this.green = g;
+        this.blue = b;
+        this.alpha = 255;
+    }
+    public int getRed()
+    {
+        return red;
+    }
+    public int getBlue()
+    {
+        return blue;
+    }
+    public int getGreen()
+    {
+        return green;
+    }
+    public int getAlpha()
+    {
+        return alpha;
+    }
+    public void setRed(int r)
+    {
+        this.red = r;
+    }
+    public void setBlue(int b)
+    {
+        this.blue = b;
+    }
+    public void setGreen(int g)
+    {
+        this.green = g;
+    }
+    public void setAlpha(int a)
+    {
+        this.alpha = a;
+    }
+    public int greyScale()
+    {
+        return (red + green + blue) / 3;
+    }
+}
---
+++ classes/classes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+class Program
+{
+    static void Main()
+    {
+        Colors c = new Colors(100, 150, 200);
+
+        Console.WriteLine(c.getRed());
+        Console.WriteLine(c.getGreen());
+        Console.WriteLine(c.getBlue());
+        Console.WriteLine(c.getAlpha());
+
+        Console.WriteLine("Grayscale: " + c.greyScale());
+
+        c.setRed(200);
+
+        Console.WriteLine("New Red: " + c.getRed());
+    }
+}
