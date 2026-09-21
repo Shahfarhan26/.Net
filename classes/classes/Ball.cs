79b9d0d2fe9e185c881a18de4f3088228ac27072
Shahfarhan26 <2468shahfarhan@gmail.com>
2026-09-21 22:38:18 +0530

    ball class

---
+++ classes/classes/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public class Ball
+{
+    private Colors color;
+    private float radius;
+    private int count;
+
+    public Ball(Colors color, float radius)
+    {
+        this.color = color;
+        this.radius = radius;
+        this.count = 0;
+    }
+    public void pop()
+    {
+        radius = 0;
+    }
+    public void Throw()
+    {
+        if(radius > 0)
+        {
+            count++;
+        }
+        
+    }
+    public int total( )
+    {
+        return count;
+    }
+}
+
--- classes/classes/Program.cs
+++ classes/classes/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main()
     {
-        Colors c = new Colors(100, 150, 200);
+        /*Colors c = new Colors(100, 150, 200);
 
         Console.WriteLine(c.getRed());
         Console.WriteLine(c.getGreen());
@@ -16,5 +16,34 @@ class Program
         c.setRed(200);
 
         Console.WriteLine("New Red: " + c.getRed());
+        */
+        // Create two different balls of different colors and sizes.
+        Ball bigRed = new Ball(new Colors(255, 0, 0), 5);
+        Ball littlePurple = new Ball(new Colors(255, 0, 255), 3);
+
+        // Throw the big red ball around a few times.
+        bigRed.Throw();
+        bigRed.Throw();
+        bigRed.Throw();
+        Console.WriteLine(bigRed.total());
+
+        // Keep throwing it, and make sure that the number of
+        // times thrown keeps going up.
+        bigRed.Throw();
+        bigRed.Throw();
+        Console.WriteLine(bigRed.total());
+
+        // Throws the little purple ball around.
+        littlePurple.Throw();
+        littlePurple.Throw();
+        Console.WriteLine(littlePurple.total());
+
+        // Pop the little purple ball and make sure that the throw
+        // count doesn't keep going up if the ball has been popped.
+        littlePurple.pop();
+        littlePurple.Throw();
+        Console.WriteLine(littlePurple.total());
+
+        Console.ReadKey();
     }
 }
