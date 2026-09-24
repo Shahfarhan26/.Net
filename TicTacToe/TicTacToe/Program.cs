374d0c80d044b59f8f6d18e6a1c29df814ab203c
Shahfarhan26 <2468shahfarhan@gmail.com>
2026-09-24 19:04:47 +0530

    for playing once game logic is complete

--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -1,13 +1,10 @@
-﻿
-using System;
-
+﻿using System;
 class Program
 {
     static void Main()
     {
         Greet();
         PvP();
-        
     }
     static void Greet()
     {
@@ -17,7 +14,6 @@ class Program
         Console.WriteLine("    Hope You Enjoy! May Your Time pass Smoothly   ");
         Dash(1);
     }
-  
     static void PvP()
     {
         Dash(2);
@@ -28,13 +24,12 @@ class Program
             P1 = "Player 1";
         }
         Dash(2);
-        Console.Write("        Player 2 Please Enter your name:          ");
+        Console.Write("        Player 2 Please Enter your name: ");
         string? P2 = Console.ReadLine();
         if (string.IsNullOrWhiteSpace(P2))
         {
             P2 = "Player 2";
         }
-
         Dash(2);
         Console.WriteLine($"        Welcome {P1} = 'X' and {P2} = 'O'        ");
         Dash(2);
@@ -49,7 +44,9 @@ class Program
         int i = 1;
         string[] xo = [ "","1", "2", "3", "4", "5", "6", "7", "8", "9" ];
         int ox;
-        while (i < 10)
+        bool exit = true;
+        bool IfWin = false;
+        while (exit)
         {
             if ((i % 2) == 0)
             {
@@ -64,8 +61,16 @@ class Program
                 Console.WriteLine($"   {P2} has choosen 'O' at {xo[ox]}th position   ");
                 xo[ox] = "O";
                 Display(xo);
-                
-            }else if((i % 2) != 0)
+                IfWin = CheckWn("O", P2, xo);
+                if (IfWin)
+                {
+                    Dash(2);
+                    Console.WriteLine("          Thanks for Playing             ");
+                    Dash(2);
+                    exit = false;
+                }
+            }
+            else if ((i % 2) != 0)
             {
                 Console.WriteLine($"{P1}'s turn. please choose 'X' position.");
                 ox = Convert.ToInt32(Console.ReadLine());
@@ -78,10 +83,43 @@ class Program
                 Console.WriteLine($"     {P1} has choosen 'X' at {xo[ox]}th position ");
                 xo[ox] = "X";
                 Display(xo);
+                IfWin = CheckWn("X", P1, xo);
+                if (IfWin)
+                {
+                    Dash(2);
+                    Console.WriteLine("          Thanks for Playing             ");
+                    Dash(2);
+                    exit = false;
+                }
             }
             i++;
-
+            if (i == 10 && (!IfWin))
+            {
+                Dash(2);
+                Console.WriteLine("          game has resulted in a Draw             ");
+                Dash(2);
+                exit = false;
+            }
         }
+    }
+    static bool CheckWn(string a,string P, string[] xo)
+    {
+        
+        if (((xo[1] == a) && (xo[2] == a) && (xo[3] == a)) ||
+                    ((xo[4] == a) && (xo[5] == a) && (xo[6] == a)) ||
+                    ((xo[7] == a) && (xo[8] == a) && (xo[9] == a)) ||
+                    ((xo[1] == a) && (xo[4] == a) && (xo[7] == a)) ||
+                    ((xo[2] == a) && (xo[5] == a) && (xo[8] == a)) ||
+                    ((xo[3] == a) && (xo[6] == a) && (xo[9] == a)) ||
+                    ((xo[1] == a) && (xo[5] == a) && (xo[9] == a)) ||
+                    ((xo[3] == a) && (xo[5] == a) && (xo[7] == a)))
+        {
+            Dash(2);
+            Console.WriteLine($"                   {P} wins                     ");
+            Dash(2);
+            return  true;
+        }
+        return false;
     }
     static void Display(string[] xo)
     {
@@ -101,7 +139,7 @@ class Program
 
         Console.WriteLine($"                -------------------");
         Dash(2);
-    }
+    } 
     static void Dash(int n)
     {
         for (int i = 0; i < n; i++)
